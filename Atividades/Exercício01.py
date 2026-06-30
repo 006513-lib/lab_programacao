c5a7ee513c09f4770f79de5d2cbd36374d89ede0
Julio Alves <006513@libertas.edu.br>
2026-06-29 23:43:55 -0300

    Delete Atividades/Exercício01.py

--- Atividades/Exercício01.py
+++
@@ -1,9 +0,0 @@
-vetor = []
-
-for i in range(10):
-    valores = int(input(f"Digite o valor número {i+1}: "))
-    vetor.append(valores)
-
-valorDif = len(set(vetor))
-
-print(f"Quantidade de números diferentes: {valorDif}")
